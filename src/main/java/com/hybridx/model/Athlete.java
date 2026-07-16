f626031b381e6a6d5186f85916ab8443ee3452df
gsnbx <swaminathan.bx@gmail.com>
2026-07-16 10:54:42 +0530

    Athlete model created

--- src/main/java/com/hybridx/model/Athlete.java
+++ src/main/java/com/hybridx/model/Athlete.java
@@ -1,5 +1,37 @@
 package com.hybridx.model;
 
 public class Athlete {
-
+	
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	
+	//Constructrs
+	
+	//getters
+	public int getId() {
+		return this.id;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public String getEmail() {
+		return this.email;
+	}
+	
+	//setters
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setEmail(String mail) {
+		this.email = mail;
+	}
+	public void setPassword(String pwd) {
+		this.password = pwd;
+	}
+	
 }
